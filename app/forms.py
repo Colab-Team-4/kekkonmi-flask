881e9816bf019909507f31cdaa4f1d18a28f3647
Hyun-Tae Jin <htjmario@gmail.com>
2023-09-20 05:17:27 -0400

    added user auth skeleton

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField()
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Create Password:', validators=[DataRequired(), Length(min=3, max=40), EqualTo('confirm', message='The passwords must match.')])
+    confirm = PasswordField('Confirm Password:', validators=[DataRequired(), EqualTo('password', message='The passwords must match.')])
+    submit = SubmitField()
