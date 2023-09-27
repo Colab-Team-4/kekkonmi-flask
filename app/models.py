23ec284b1c297b3a29c26b1c60f18cf4493ac5e7
Jose Laurito <124356021+jigolaurito93@users.noreply.github.com>
2023-09-27 00:32:27 -0500

    changed Log In and Log Out (#2)
    * changed Log In and Log Out
    
    * schema for Venue and Event
    
    * added __init__ and __repr__ for Venue and Event
    
    * removed the comment & changed the object type for total_price under class Event, price under class Venue
    
    * removed the comment & changed the object type for total_price under class Event, price under class Venue

--- app/models.py
+++ app/models.py
@@ -37,3 +37,41 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'email: {self.email} added to Users'
+
+class Venue(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    price = db.Column(db.Numeric(precision=10, scale=2))
+    location = db.Column(db.String(150), nullable=False)
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
+
+    def __init__(self, price, location, user_token):
+        self.id = self.set_id()
+        self.price = price
+        self.location = location
+        self.user_token = user_token
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+        
+    def __repr__(self):
+        return f'Price: ${self.price} | Location: {self.location}'
+
+class Event(db.Model):
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    total_price = db.Column(db.Numeric(precision=10, scale=2))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
+
+    def __init__(self, start_date, end_date, total_price, user_token):
+        self.id = self.set_id()
+        self.start_date = start_date
+        self.end_date = end_date
+        self.total_price = total_price
+        self.user_token = user_token
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+    def __repr__(self):
+        return f'From {self.start_date} to {self.end_date} | Total Cost: ${self.total_price}'
+
